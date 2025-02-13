aab313d0a8272319ed39ed9f662421de2e41a08a
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-13 17:20:54 -0400

    Update index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
+import { useErrorMiddleware, useSuccessResponseMiddleware } from "@/middlewares/responses.ts";
 import entities from "./entities/index.ts";
 
 import { Elysia } from "elysia";
 
-export const routes = new Elysia({ prefix: "api" }).use(entities);
+export const routes = new Elysia({ prefix: "api" }).use(useSuccessResponseMiddleware).use(useErrorMiddleware).use(entities);
