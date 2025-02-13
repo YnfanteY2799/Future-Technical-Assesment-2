5e3ca73cd16b9ab7a46fa787dd9bf8d9049ceccc
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-13 18:08:30 -0400

    +adds

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,26 +1,3 @@
-interface IBaseEntity {
-  friendly_name: string;
-  last_reported: string;
-  last_updated: string;
-  last_changed: string;
-  client_id: string;
-  entity_id: string;
-}
-
-export interface ILightEntity extends IBaseEntity {
-  state: "on" | "off";
-  brightness: number;
-  type: "light";
-}
-
-export interface ISensor extends IBaseEntity {
-  unit_of_measurement: string | number;
-  type: "sensor";
-}
-
-export interface ISwitch extends IBaseEntity {
-  state: "on" | "off" | "unavailable";
-  type: "switch";
-}
+import type { ILightEntity, ISensor, ISwitch } from "@/types/db";
 
 export const memoryEntities: Array<ILightEntity | ISensor | ISwitch> = [];
---
+++ src/types/db.ts
@@ -0,0 +1,24 @@
+export interface IBaseEntity {
+  friendly_name: string;
+  last_reported: string;
+  last_updated: string;
+  last_changed: string;
+  client_id: string;
+  entity_id: string;
+}
+
+export interface ILightEntity extends IBaseEntity {
+  state: "on" | "off";
+  brightness: number;
+  type: "light";
+}
+
+export interface ISensor extends IBaseEntity {
+  unit_of_measurement: string | number;
+  type: "sensor";
+}
+
+export interface ISwitch extends IBaseEntity {
+  state: "on" | "off" | "unavailable";
+  type: "switch";
+}
