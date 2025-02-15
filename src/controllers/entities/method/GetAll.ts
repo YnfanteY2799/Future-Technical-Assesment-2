8ee76921347e23a281dce846c83c26f26a66873d
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-14 23:08:04 -0400

    +adds

--- src/controllers/entities/method/GetAll.ts
+++ src/controllers/entities/method/GetAll.ts
@@ -1,5 +1,6 @@
 import { GetAllQueryDTO } from "../dtos";
 import { memoryEntities } from "@/db";
+import { NotFoundException } from "@/utils/error";
 
 import { Elysia } from "elysia";
 
@@ -9,8 +10,9 @@ export default new Elysia().get(
     try {
       const data = memoryEntities.filter(({ type, client_id, entity_id, friendly_name }) => {});
 
-      if (!data) ;
+      if (!data) throw new NotFoundException("There are no registers!");
 
+      
       return data;
     } catch (e) {
       console.error("Error on create method : ", e);
