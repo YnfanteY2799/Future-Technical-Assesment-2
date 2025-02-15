12bec166db8611c0d514b3f9989cc8f5aa482582
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-14 23:46:41 -0400

    Update Create.ts

--- src/controllers/entities/method/Create.ts
+++ src/controllers/entities/method/Create.ts
@@ -3,22 +3,54 @@ import { CreateEntityDTO } from "../dtos";
 
 import { Elysia } from "elysia";
 
+import type { ILightEntity, ISensor, ISwitch } from "@/types/db";
+import { modifyMemoryEntities } from "@/db";
+
 export default new Elysia().post(
   "Create",
   async ({ body: { type } }) => {
     try {
       switch (type) {
         case "ligth":
-          const ligth = {};
-
+          const ligth: ILightEntity = {
+            state: "on",
+            brightness: 0,
+            type: "ligth",
+            friendly_name: "",
+            last_reported: "",
+            last_updated: "",
+            last_changed: "",
+            client_id: "",
+            entity_id: "",
+          };
+          modifyMemoryEntities([ligth], "Add");
           break;
         case "sensor":
-          const sensor = {};
+          const sensor: ISensor = {
+            unit_of_measurement: "",
+            type: "sensor",
+            friendly_name: "",
+            client_id: "",
+            entity_id: "",
+            last_reported: "",
+            last_updated: "",
+            last_changed: "",
+          };
 
+          modifyMemoryEntities([sensor], "Add");
           break;
         case "switch":
-          const switch_data = {};
-
+          const switch_data: ISwitch = {
+            state: "on",
+            type: "switch",
+            friendly_name: "",
+            last_reported: "",
+            last_updated: "",
+            last_changed: "",
+            client_id: "",
+            entity_id: "",
+          };
+          modifyMemoryEntities([switch_data], "Add");
           break;
         default:
           throw new BadRequestException("Not a valid type");
