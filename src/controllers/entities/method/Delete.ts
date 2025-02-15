a4aeafb53066892bf581038feac21acd92302b74
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2025-02-14 23:30:17 -0400

    +adds

--- src/controllers/entities/method/Delete.ts
+++ src/controllers/entities/method/Delete.ts
@@ -1,5 +1,5 @@
+import { getMemoryEntities, modifyMemoryEntities } from "@/db";
 import { CommonIDParamsDTO } from "../dtos";
-import { memoryEntities } from "@/db";
 
 import { Elysia } from "elysia";
 
@@ -7,9 +7,9 @@ export default new Elysia().delete(
   "Delete/:id",
   async ({ params: { id } }) => {
     try {
-      const newData = memoryEntities.filter(({ entity_id }) => entity_id !== id);
-
-      memoryEntities = newData;
+      const newData = getMemoryEntities().filter(({ entity_id }) => entity_id !== id);
+      modifyMemoryEntities(newData, "Delete");
+      return getMemoryEntities();
     } catch (e) {
       console.error("Error on delete method : ", e);
       throw e;
--- src/controllers/entities/method/GetById.ts
+++ src/controllers/entities/method/GetById.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from "@/utils/error";
 import { CommonIDParamsDTO } from "../dtos";
+import { getMemoryEntities } from "@/db";
 
 import { Elysia } from "elysia";
 
@@ -6,8 +8,9 @@ export default new Elysia().get(
   "Get/:id",
   async ({ params: { id } }) => {
     try {
-
-
+      const data = getMemoryEntities().filter(({ entity_id }) => entity_id === id);
+      if (!data) throw new NotFoundException("There are no registers!");
+      return data[0];
     } catch (e) {
       console.error("Error on create method : ", e);
       throw e;
